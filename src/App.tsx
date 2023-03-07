837c059f048d55e01ae2ee95591fc6fa17e7bf81
JunYoungKim <kjy@imtsoft.org>
2023-03-07 17:02:14 +0900

    update abc d

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,7 @@ function App() {
   const b=2
   const c=3
   const d=4
-  const e=5
-  const f=6
-  const g=7
-  const h=8
-  const i=9
+
   return (
     <div className="App">
       <header className="App-header">
